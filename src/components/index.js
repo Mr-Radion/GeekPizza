649692da5b0a63e374ec0ecc4ed5062774962420
Mr.Radion <mr.rodion_oa@mail.ru>
2020-11-06 18:51:06 +0300

    Creating the SortPopup component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './Header'; // import + export in file Header rename default as Header
 export { default as Categories } from './Categories';
 export { default as Button } from './Button';
+export { default as SortPopup } from './SortPopup';
