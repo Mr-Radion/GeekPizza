6feb4a3063b5cae7226b94a584bca51e7532449d
Mr.Radion <mr.rodion_oa@mail.ru>
2020-12-13 14:05:49 +0300

    Implemented React.memo and React.useCallback

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import { Categories, SortPopup, PizzaBlock } from '../components';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCategory } from '../redux/actions/filters';
+
+const categoriesName = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+const sortItems = [
+  { name: 'популярности', type: 'popular' },
+  { name: 'цене', type: 'price' },
+  { name: 'алфавиту', type: 'alphabet' },
+];
 
 function Home() {
-  const state = useSelector(({ pizzas }) => {
-    return {
-      items: pizzas.items
-    }
-  });
+  const dispatch = useDispatch();
+  const items = useSelector(({ pizzas }) => pizzas.items);
+
+  const onSelectCategory = React.useCallback((index) => {
+    dispatch(setCategory(index));
+  }, []);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories
-          onClickItem={(name) => console.log(name)}
-          items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']}
-        />
-        <SortPopup items={[{name:'популярности', type: 'popular'}, {name:'цене', type: 'price'}, {name:'алфавиту', type:'alphabet'}]} />
+        <Categories onClickItem={onSelectCategory} items={categoriesName} />
+        <SortPopup items={sortItems} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {state.items &&
-          state.items.map((obj) => (
-            <PizzaBlock key={obj.id} {...obj} />
-          ))}
+        {items && items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
     </div>
   );
