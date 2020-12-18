5453a733ee8a1ddb23e36b3bcb143d6225a52d50
Mr.Radion <mr.rodion_oa@mail.ru>
2020-12-18 20:47:08 +0300

    Adding middleware redux.thunk

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -1,3 +1,14 @@
+import axios from 'axios';
+// import { fetchPizzasApi } from "../../api";
+
+export const fetchPizzas = () => (dispatch) => {
+  axios.get('http://localhost:3001/db.json').then(({ data }) => {
+    dispatch(setPizzas(data.pizzas));
+  });
+
+  // fetchPizzasApi().then();
+};
+
 export const setPizzas = (items) => ({
   type: 'SET_PIZZAS',
   payload: items,
